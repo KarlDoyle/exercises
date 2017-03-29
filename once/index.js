3df2013d3ceb442340b3fe1398e9f9cd21bb79d1
KarlDoyle <karldoyle@outlook.com>
2017-03-29 20:30:43 +0100

    once answer solved

---
+++ once/index.js
@@ -0,0 +1,14 @@
+let once = function(func) {
+  let logger = [];
+  let result;
+  return function() {
+    if (logger.indexOf(func) === -1) {
+      logger.push(func);
+      return result = func.apply(this, arguments)
+    } else {
+      return result
+    }
+  }
+}
+
+module.exports = once;
